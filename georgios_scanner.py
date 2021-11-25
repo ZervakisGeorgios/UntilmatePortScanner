5629019eba6a449c2c0cf06a9e6ac4b50332d2e7
Georgios Zervakis <zervakisgiorgos91@gmail.com>
2021-11-25 21:17:45 +0000

    Update georgios_scanner.py
    Created open_file function and placeholder validate_subnet

--- georgios_scanner.py
+++ georgios_scanner.py
@@ -1,7 +1,8 @@
 import ipaddress
+import sys
 
 # A variable to store the IP addresses when converted from a subnet. Used from "subnet_to_ip" function
-list_of_ip_addresses = []
+# list_of_ip_addresses = []
 
 
 def usage():
@@ -41,6 +42,7 @@ def usage():
 def validate_ip_address(address):
     '''
     This function will be used to validate that the IP address the user passed is a valid one
+    source: https://codefather.tech/blog/validate-ip-address-python/
     :param address: It will be arg[1] passed directly as a CML option
     :return: Returns True if it is a valid IPv4/v6 or False if it is not a valid IPv4/v6
     '''
@@ -56,10 +58,11 @@ def validate_ip_address(address):
 def subnet_to_ip(subnet_address):
     '''
     This function will produce a list of IP addresses should the user passes a subnet as an input
+
     :param subnet_address: The subnet that the user passed via the txt file
     :return: The function will return a list of the IP addresses based on the given subnet
     '''
-    global list_of_ip_addresses
+    list_of_ip_addresses = []
     #A for loop will loop all IP addresses from a given subnet
     for address in ipaddress.ip_network(subnet_address):
         # Each IP address will be converted to a string and appended into the list
@@ -68,4 +71,30 @@ def subnet_to_ip(subnet_address):
     return list_of_ip_addresses
 
 
+def validate_subnet(list_of_ip_addresses):
+    pass
 
+
+def open_file(file_to_open):
+    '''
+    This fucntion will open a file to read the IP addresses that the user provided
+    :param file_to_open: The name or the full path of the file to read IP addresses from
+    :return: It returns the list of the provided IP addresses
+    '''
+    list_of_ip_addresses = []
+    try:
+        with open(file_to_open) as f:
+            # contents = f.readlines() #This will provide liens with the \n as suffix: ['1.1.1.1\n', '2.2.2.2']
+            list_of_ip_addresses = f.read().splitlines()  # It will store the actual values without the \n
+        f.close()
+
+        return list_of_ip_addresses
+    except IOError:
+        print("******************************************************************************")
+        print("************************COULD NOT OPEN THE FILE*******************************")
+        print("******************************************************************************")
+        usage()
+        sys.exit()
+
+
+open_file("ip_addresses.txt")
