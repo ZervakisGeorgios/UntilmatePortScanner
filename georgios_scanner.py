ed2040dafd2a7b0cb3bfc3f3cac7f2cbad3551c4
Georgios Zervakis <zervakisgiorgos91@gmail.com>
2021-11-26 19:09:57 +0000

    Update georgios_scanner.py
    Created wrong_arguments definition, Created the write to json file function and started the main

--- georgios_scanner.py
+++ georgios_scanner.py
@@ -1,14 +1,13 @@
 import ipaddress
 import sys
-
-# A variable to store the IP addresses when converted from a subnet. Used from "subnet_to_ip" function
-# list_of_ip_addresses = []
+import json
+import time
 
 
 def usage():
-    '''
+    """
     The method usage prints out the banner of the script as well as examples of the usage and information about The Creator
-    '''
+    """
     print('''
 
   ________                          .__                       __________   
@@ -39,32 +38,35 @@ def usage():
     TryHackMe: DrG
     ''')
 
+
 def validate_ip_address(address):
-    '''
+    """
     This function will be used to validate that the IP address the user passed is a valid one
     source: https://codefather.tech/blog/validate-ip-address-python/
     :param address: It will be arg[1] passed directly as a CML option
     :return: Returns True if it is a valid IPv4/v6 or False if it is not a valid IPv4/v6
-    '''
+    """
     try:
         ip = ipaddress.ip_address(address)
         return True
-        # print("IP address {} is valid. The object returned is {}".format(address, ip))
     except ValueError:
-        return False
-        # print("IP address {} is not valid".format(address))
+        print("**************************************************************************************************")
+        print("*************************************** Invalid IP address ***************************************")
+        print("**************************************************************************************************")
+        usage()
+        sys.exit()
 
 
 def subnet_to_ip(subnet_address):
-    '''
+    """
     This function will produce a list of IP addresses should the user passes a subnet as an input. It will flag and exit
     if the subnet is not valid.
 
     :param subnet_address: The subnet that the user passed via the txt file
     :return: The function will return a list of the IP addresses based on the given subnet
-    '''
+    """
     list_of_ip_addresses = []
-    #A for loop will loop all IP addresses from a given subnet
+    # A for loop will loop all IP addresses from a given subnet
     try:
         for address in ipaddress.ip_network(subnet_address):
             # Each IP address will be converted to a string and appended into the list
@@ -80,11 +82,11 @@ def subnet_to_ip(subnet_address):
 
 
 def open_file(file_to_open):
-    '''
+    """
     This fucntion will open a file to read the IP addresses that the user provided
     :param file_to_open: The name or the full path of the file to read IP addresses from
     :return: It returns the list of the provided IP addresses
-    '''
+    """
     list_of_ip_addresses = []
     try:
         with open(file_to_open) as f:
@@ -101,4 +103,45 @@ def open_file(file_to_open):
         sys.exit()
 
 
-open_file("ip_addresses.txt")
+def write_json_file(data_to_write):
+    """
+    This function writes json data into a file.
+    source: https://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file
+    :param data_to_write: The json data to be written in the file
+    :return: Returns True if the script managed to write the file
+    """
+    try:
+        with open('output.txt', 'w', encoding='utf-8') as outfile:
+            json.dump(data_to_write, outfile, ensure_ascii=False, indent=4)
+
+        return True
+    except IOError:
+        print("********************************************************************************")
+        print("*** Unexpected error while opening the output.txt file to write the contents ***")
+        print("********************************************************************************")
+        usage()
+        sys.exit()
+
+
+def wrong_arguments():
+    print("************************************************************************************")
+    print("* You have not provided the correct number of arguments. Or not expected arguments *")
+    print("* were received.                                                                   *")
+    print("* Check the examples below                                                         *")
+    print("************************************************************************************")
+
+
+if __name__ == '__main__':
+    usage()  # The banner is called
+    print(f'first argument {sys.argv[2]}')
+    print(f'Second argument {sys.argv[3]}')
+    print(len(sys.argv))
+    # If the number of arguments passed is not equal to 4 or the arguments are not correct, the script terminates
+    if (len(sys.argv) != 4) or (sys.argv[2] not in ("-t", "-u")) or (sys.argv[3] not in ("-b", "i")):
+        wrong_arguments()   # It informs the user that the wrong arguments were passed
+        usage()             # It will show the banner with the examples to the user again
+        sys.exit()          # The script terminates
+    elif sys.argv[1] in ("help", "-h", "h", "?", "--h", "--help", "/?"):
+        usage()
+        sys.exit()
+    
